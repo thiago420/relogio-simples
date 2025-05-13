25378b1f69f337cd186a5c2741a494f707c967c1
Thiago <thiagoamthypc@gmail.com>
2025-05-13 12:29:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,171 @@
+// TIPO 1: Formatação simples TIPO 2: Formatação avançada
+
+let dateToday;
+let h;
+let min;
+let s;
+let d;
+let dw;
+let m;
+let mn;
+let y;
+
+const horario = document.getElementById('horario');
+const data = document.getElementById('data');
+const dataDiv = document.getElementById('dataDiv');
+
+let formatacaoAvancado = true;
+
+let fusoHorario;
+
+const relogio = setInterval(function time() {
+    dateToday = new Date();
+    h = String(dateToday.getHours()).padStart(2, '0'); // Hora
+    min = String(dateToday.getMinutes()).padStart(2, '0'); // Minuto
+    s = String(dateToday.getSeconds()).padStart(2, '0'); // Segundo
+    d = String(dateToday.getDate()).padStart(2, '0'); // Dia
+    dw = dateToday.getDay(); // Dia da Semana (0 a 6)
+    m = String(dateToday.getMonth() + 1).padStart(2, '0'); // Mês
+    mn = dateToday.getMonth(); // Nome dos Meses (0 a 11)
+    y = dateToday.getFullYear(); // Ano
+
+    switch(dw) {
+        case 0:
+            dw = "Domingo";
+            break;
+        case 1:
+            dw = "Segunda-feira";
+            break;
+        case 2:
+            dw = "Terça-feira";
+            break;
+        case 3:
+            dw = "Quarta-feira";
+            break;
+        case 4:
+            dw = "Quinta-feira";
+            break;
+        case 5:
+            dw = "Sexta-feira";
+            break;
+        case 6:
+            dw = "Sábado";
+            break;
+    }
+
+    switch(mn) {
+        case 0:
+            mn = "Janeiro";
+            break;
+        case 1:
+            mn = "Fevereiro";
+            break;
+        case 2:
+            mn = "Março";
+            break;
+        case 3:
+            mn = "Abril";
+            break;
+        case 4:
+            mn = "Maio";
+            break;
+        case 5:
+            mn = "Junho";
+            break;
+        case 6:
+            mn = "Julho";
+            break;
+        case 7:
+            mn = "Agosto";
+            break;
+        case 8:
+            mn = "Setembro";
+            break;
+        case 9:
+            mn = "Outubro";
+            break;
+        case 10:
+            mn = "Novembro";
+            break;
+        case 11:
+            mn = "Dezembro";
+            break;
+    }
+
+    atualizarHorario();
+    // atualizarFuso();
+
+});
+
+function atualizarHorario() {
+    horario.textContent = h + ":" + min + ":" + s;
+
+    if (formatacaoAvancado) {
+        data.textContent = dw + ", " + d + " de " + mn + " de " + y;
+    } else {
+        data.textContent = d + "/" + m + "/" + y;
+    }
+}
+
+function trocarData() {
+    if (formatacaoAvancado) {
+        formatacaoAvancado = false;
+        atualizarHorario();
+        dataDiv.className = "data simples";
+        dataDiv.setAttribute("title", "Formatação: Simples");
+        document.getElementById("formatacaoBtn").value = "Simples";
+    } else {
+        formatacaoAvancado = true;
+        atualizarHorario();
+        dataDiv.className = "data avancado";
+        dataDiv.setAttribute("title", "Formatação: Avançado");
+        document.getElementById("formatacaoBtn").value = "Avançado";
+    }
+}
+
+function abrirConfig(abrir) {
+    if (abrir) {
+        document.getElementById("configBtn").className = "configBtn esconder";
+        document.getElementById("configJanela").className = "configJanela";
+    } else {
+        document.getElementById("configJanela").className = "configJanela esconder";
+        document.getElementById("configBtn").className = "configBtn";
+    }
+}
+
+function trocarCor(cor, tipo) {
+    switch (tipo) {
+        case "fundo":
+            document.body.style.backgroundColor = cor;
+            break;
+        case "letra":
+            elementos = document.getElementsByClassName("data");
+            for (i = 0; i < elementos.length; i++) {
+                elementos[i].style.color = cor;
+            }
+            break;
+        case "fundoRedefinir":
+            document.body.style.backgroundColor = "#000000";
+            document.getElementById("inputFundo").value = "#000000";
+            break;
+        case "letraRedefinir":
+            elementos = document.getElementsByClassName("data");
+            for (i = 0; i < elementos.length; i++) {
+                elementos[i].style.color = "#3d3d3d";
+            }
+            document.getElementById("inputLetra").value = "#3d3d3d";
+            break;
+    }
+}
+
+// function atualizarFuso() {
+//     let calculo = dateToday.getHours() - dateToday.getUTCHours();
+//     if (calculo == 0) {
+//         fusoHorario = "UTC";
+//     } else if (calculo > 0) {
+//         fusoHorario = "UTC+" + calculo;
+//     } else if (calculo < 0) {
+//         fusoHorario = "UTC" + calculo;
+//     }
+//     document.getElementById("fuso").innerHTML = fusoHorario;
+// }
